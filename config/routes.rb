3578a637907d9d53077901975bf7089406ec7c31
Maxime Braud <mx.braud@gmail.com>
2015-06-18 17:40:19 +0200

    css to index show and new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'cocktails#index'
+
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :show]
   end
